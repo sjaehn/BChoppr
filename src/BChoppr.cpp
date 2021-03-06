a5585160d0fec8c303f318a409ad136afd3c15bc
sjaehn <kontakt@jahnichen.de>
2021-03-06 22:31:10 +0100

    Check nullptr

--- src/BChoppr.cpp
+++ src/BChoppr.cpp
@@ -667,13 +667,13 @@ static LV2_Handle instantiate (const LV2_Descriptor* descriptor, double samplera
 static void connect_port (LV2_Handle instance, uint32_t port, void *data)
 {
 	BChoppr* inst = (BChoppr*) instance;
-	inst->connect_port (port, data);
+	if (inst) inst->connect_port (port, data);
 }
 
 static void run (LV2_Handle instance, uint32_t n_samples)
 {
 	BChoppr* inst = (BChoppr*) instance;
-	inst->run (n_samples);
+	if (inst) inst->run (n_samples);
 }
 
 static LV2_State_Status state_save(LV2_Handle instance, LV2_State_Store_Function store, LV2_State_Handle handle, uint32_t flags,
@@ -697,7 +697,7 @@ static LV2_State_Status state_restore(LV2_Handle instance, LV2_State_Retrieve_Fu
 static void cleanup (LV2_Handle instance)
 {
 	BChoppr* inst = (BChoppr*) instance;
-	delete inst;
+	if (inst) delete inst;
 }
 
 static const void* extension_data(const char* uri)
